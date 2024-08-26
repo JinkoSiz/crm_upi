91df38a387c37bcccb96b34f252d60da173b37cd
Zakhar Sotnichenko <zpska1977@mail.ru>
2024-08-26 20:21:01 +0300

    vars

--- crm_upi/settings.py
+++ crm_upi/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = DJANGO_KEY
+SECRET_KEY = os.environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -151,6 +151,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True 
-EMAIL_HOST_USER = EMAIL_USER  # Your email address
-EMAIL_HOST_PASSWORD = EMAIL_PASSWORD  # Your email account password
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')  # Your email address
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')  # Your email account password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
