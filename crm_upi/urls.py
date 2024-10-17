9a2f637c108fc192becb7ac3064640b76320a709
Zakhar Sotnichenko <zpska1977@mail.ru>
2024-10-17 20:51:04 +0300

    Added debug + fixing projects

--- crm_upi/urls.py
+++ crm_upi/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('task_manager.urls')),  # Подключаем URLs из task_manager
-]
+] + debug_toolbar_urls()
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
