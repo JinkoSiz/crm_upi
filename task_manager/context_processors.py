fa681e827a9f675d9cef07ba85fa0595fa25338c
JinkoSiz <zakhar.sotnichenko@mail.ru>
2024-11-26 20:42:54 +0300

    Adding smth

--- task_manager/context_processors.py
+++ task_manager/context_processors.py
@@ -1,4 +1,4 @@
-from task_manager.models import CustomUser
+from task_manager.models import *
 
 
 def all_users(request):
@@ -6,4 +6,19 @@ def all_users(request):
     Контекстный процессор для добавления всех пользователей в контекст.
     """
     all_users = CustomUser.objects.all()  # Получаем всех пользователей
-    return {'all_users': all_users}
+    all_roles = Role.objects.all()
+    all_departments = Department.objects.all()
+    all_projects = Project.objects.all()
+    all_sections = Section.objects.all()
+    all_buildings = Building.objects.all()
+    all_marks = Mark.objects.all()
+    all_tasks = TaskType.objects.all()
+    return {'all_users': all_users,
+            'all_roles': all_roles,
+            'all_departments': all_departments,
+            'all_projects': all_projects,
+            'all_sections': all_sections,
+            'all_buildings': all_buildings,
+            'all_marks': all_marks,
+            'all_tasks': all_tasks,
+            }
