ebc28c8cb897aa99e46162273c288562fd87ed2a
JinkoSiz <zpska1977@mail.ru>
2025-03-20 16:58:25 +0300

    Fix for lots of shit

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('users/<uuid:pk>/delete/', views.deleteUser, name='user-delete'),
     path('users/<uuid:pk>/invite/', views.send_invitation, name='send-invitation'),
     path('users/<uuid:pk>/reset-password', views.reset_password, name='reset-password'),
+    path('users/check_user_exists/', views.check_user_exists, name='check-user-exists'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
 
